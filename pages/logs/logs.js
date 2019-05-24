4750bdccb6f5242437eba347a52ffc82795901f7
isuperflying <1365961935@qq.com>
2019-05-24 16:28:13 +0800

    fix bugs

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -6,10 +6,16 @@ Page({
     logs: []
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
+    // this.setData({
+    //   logs: (wx.getStorageSync('logs') || []).map(log => {
+    //     return util.formatTime(new Date(log))
+    //   })
+    // })
+    wx.request({
+      url: 'http://t.yushu.im/v2/movie/top250',
+      success:function(res){
+        console.log(res.data)
+      }
     })
   }
 })
